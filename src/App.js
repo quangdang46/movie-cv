786285079781cc92ff41e63e4947a72f2e51237a
quangdang46 <tranquangdang21@gmail.com>
2022-11-28 20:20:34 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-3xl font-bold text-secondary">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum at
+      consectetur corporis iste illum, sed odit maiores deleniti optio totam,
+      sint cum adipisci quas! Numquam nostrum illum ea accusamus dolores.
+    </div>
+  );
+}
+
+export default App;
